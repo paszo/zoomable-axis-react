38161f831d48a37eeb3396ec53e9dc3e5a70e927
paszo <paszo2008@gmail.com>
2022-11-17 10:47:12 +0100

    workinggit add .!

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -2,104 +2,89 @@ import * as d3 from "d3";
 import React, { useState, useEffect, useRef } from "react";
 
 function Chart() {
-  // const ref = React.createRef();
-    const ref = useRef();
 
-  const [fill, setFill] = useState("red");
-  const [zoomState, setZoomState] = useState();
+    const svgRef = useRef();
 
+    function toggleFill() {
+        if (fill === "red") {
+            setFill("blue");
+        } else {
+            setFill("red");
+        }
+    }
 
-  const width = 500;
-  const height = 400;
-  const margin = { top: 50, right: 50, bottom: 50, left: 100 };
-  const innerHeight = height - margin.top - margin.bottom;
+    const [fill, setFill] = useState("red");
+    const [zoomState, setZoomState] = useState();
+    const width = 500;
+    const height = 400;
+    const margin = { top: 50, right: 50, bottom: 50, left: 100 };
+    const innerHeight = height - margin.top - margin.bottom;
 
-  const data = [
-    [-100, 100],
-    [-100, 200],
-    [-100, 300],
-    [-100, 400]
-  ];
+    const data = [
+        [-100, 100],
+        [-100, 200],
+        [-100, 300],
+        [-100, 400]
+    ];
+    useEffect(() => draw(),[fill, zoomState]);
+    function draw() {
 
-  useEffect(() => draw(),[fill, zoomState]);
+        let yScale = d3.scaleLinear().domain([100, 400]).range([innerHeight, 0]);
 
-  function draw() {
+        if(zoomState){
+            const newYscale = zoomState.rescaleY(yScale);
+            yScale.domain(newYscale.domain());
+        }
 
+        let yAxis = d3.axisLeft(yScale);
 
-      let yScale = d3.scaleLinear().domain([100, 400]).range([innerHeight, 0]);
+        const zoomer = d3.zoom().on("zoom", zoom);
 
-      if(zoomState){
-          const newYscale = zoomState.rescaleY(yScale);
-          yScale.domain(newYscale.domain());
-      }
+        function zoom(event) {
+            const zoomState = event.transform;
+            setZoomState(zoomState);
+        }
 
+        const svg = d3
+            .select(svgRef.current)
+            .attr("width", width)
+            .attr("height", height)
+            .call(zoomer);
 
-          let yAxis = d3.axisLeft(yScale);
+        svg.select("text.button")
+            .attr("transform", "translate(200,20)")
+            .on("click", toggleFill);
 
-          d3.select(ref.current).selectAll("svg").remove();
+        svg.select("#circles-group")
+            .attr("transform", "translate(200, 0)")
+            .selectAll("circle")
+            .data(data)
+            .join("circle")
+            .style("fill", fill)
+            .attr("r", 4)
+            .attr("cx", function (d) {
+                return d[0];
+            })
+            .attr("cy", function (d) {
+                return yScale(d[1]);
+            });
 
-          const zoomer = d3.zoom().on("zoom", zoom);
+        svg.select("#y-axis")
+            .attr("transform", "translate(75,0)")
+            .call(yAxis);
+    }
 
-          function zoom(event) {
-                // console.log(event);
-                const zoomState = event.transform;
-                setZoomState(zoomState);
-          }
+    return (
+        <div style={{margin: 50, position: "relative"}} className="OptionsDataChart">
+           <svg ref={svgRef}>
+               <text className="button">toggle fill</text>
+               <g id="circles-group"/>
+               <g id="y-axis"/>
+           </svg>
+        </div>
 
-          const svg = d3
-              .select(ref.current)
-              .append("svg")
-              .attr("width", width)
-              .attr("height", height)
-              .call(zoomer);
-
-          svg
-              .append("text")
-              .attr("transform", "translate(200,20)")
-              .text("toggle fill")
-              .on("click", toggleFill);
-
-          function toggleFill() {
-              if (fill === "red") {
-                  setFill("blue");
-              } else {
-                  setFill("red");
-              }
-          }
-
-          svg
-              .append("g")
-              .attr("id", "circles")
-              .attr("transform", "translate(200, 0)")
-              .selectAll("circle")
-              .data(data)
-              .enter()
-              .append("circle")
-              .style("fill", fill)
-              .attr("r", 4)
-              .attr("cx", function (d) {
-                  return d[0];
-              })
-              .attr("cy", function (d) {
-                  return yScale(d[1]);
-              });
-
-          svg
-              .append("g")
-              .attr("id", "y_axis")
-              .attr("transform", "translate(75,0)")
-              .call(yAxis);
-
-
-      }
-
-  return (
-    <div
-      style={{ margin: 50, position: "relative" }}
-      className="OptionsDataChart"
-      ref={ref}
-    />
-  );
+    )
 }
 
 export default Chart;
+
